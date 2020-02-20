061713ae1857a48f54f3eb5d7fcc4a2dadcdb5eb
Daniel Sasser <danielbsasser@gmail.com>
2020-02-20 21:12:19 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,6 +3,15 @@ class Appointment
   
   @@all = []
   
-  def initialize(date, )
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
   
 end
